8b660240db031a323234714fb3099c33a13e051e
Viljo Karhumaa <viljo.karhumaa@gmail.com>
2019-08-22 10:40:04 +0300

    3.13 & 3.14

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const express       = require('express')
 const app           = express()
 const bodyParser    = require('body-parser')
 const morgan        = require('morgan')
 const cors          = require('cors')
+const Person        = require('./models/person')
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -25,91 +27,68 @@ app.use(morgan((tokens, req, res) => {
 
 app.use(express.static('build'))
 
-let persons = []
-
 app.get('/', (req, res) => {
     res.send('Hello world')
 })
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(persons => {
+        res.status(200).json(persons.map(person => person.toJSON()))
+    })
 })
 
 app.post('/api/persons', (req, res) => {
     const data = req.body
     if (!data.name || !data.number) {
-        res.statusCode = 422
-        res.send({error : 'Nimi ja/tai numero puuttuu'})
+        return res.status(422).json({error : 'Nimi ja/tai numero puuttuu'})
     }
-    else if (persons.find(person => person.name === data.name)) {
-        res.statusCode = 422
-        res.send({error : `Henkilö ${data.name} löytyy jo luettelosta`})
-    }
-    else {
-        let id = Math.ceil(Math.random() * 1000)
-        while (persons.find(person => person.id === id)) {
-            console.log('Arvotaan uusi tunniste')
-            id = Math.ceil(Math.random() * 1000)
-        }
-        console.log(id, req.body.name, req.body.number)
-        const newPerson = {
-            id      :   id,
-            name    :   req.body.name,
-            number  :   req.body.number
-        }
-        persons.push(newPerson)
-        res.json(newPerson)
-    }
+    const person = new Person({
+        name:   data.name,
+        number: data.number,
+    })
+    person.save().then(savedPerson => {
+        res.status(200).json(savedPerson.toJSON())
+    }).catch(error => {
+        res.status(421).json({error: error.message})
+    })
 })
 
 app.get('/api/persons/:id', (req, res) => {
     const id = parseInt(req.params.id)
-    console.log(`Pyydetty id: ${id}`)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        res.json(person)
-    }
-    else {
-        res.statusCode = 422
-        res.send('Pyydetyllä id:llä ei löytynyt tietoja')
-    }
+    Person.findById(id).then(person => {
+        res.status(200).json(person.toJSON())
+    }).catch(error => {
+        res.status(421).json({error: error.message})
+    })
 })
 
 app.put('/api/persons/:id', (req,res) => {
-    console.log(req.body)
-    const id = parseInt(req.params.id)
-    console.log(id)
-    const index = persons.findIndex(person => person.id === id)
-    if (index !== -1) {
-        persons[index].number = req.body.number
-        res.send(persons[index])
-    }
-    else {
-        res.statusCode = 422
-        res.send({error : `No matching persons with id: ${req.params.id}`})
-    }
+    const id = req.params.id
+    Person.updateOne({_id: id}, {number: req.body.number}).then(updateRes => {
+        res.status(200).json({error: 0, message: 'Update successfull'})
+    }).catch(error => {
+        res.status(421).json({error: error.message})
+    })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = parseInt(req.params.id)
-    console.log(`Pyydetty poistamaan id: ${id}`)
-    const personToDelete = persons.find(person => person.id === id)
-    if (personToDelete) {
-        persons = persons.filter(person => person.id !== id)
-        res.send(`Poistettiin ${personToDelete.name}`)
-    }
-    else {
-        res.statusCode = 404
-        res.send('Poistettavaa henkilöä ei löydy')
-    }
+    const id = req.params.id
+    Person.deleteOne({_id: id}).then(delRes => {
+        res.json(delRes)
+    }).catch(error => {
+        res.status(421).json({error: error.message})
+    })
 })
 
 app.get('/info', (req, res) => {
     const time = new Date().toLocaleString()
-    res.send(`Puhelinluettelossa on ${persons.length} nimeä \n${time}`)
+    Person.countDocuments().then(count => {
+        res.status(200).json({count: count, time: time})
+    }).catch(error => {
+        res.status(421).json({error: error.message})
+    })
 })
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-    console.log(`Server running at port ${PORT}`)
+app.listen(process.env.PORT, () => {
+    console.log(`Server running at port ${process.env.PORT}`)
 })
